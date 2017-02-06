9dd49ce35b4753930996104033d4cea0e75740f0
azheng51714 <414294494@qq.com>
2017-02-06 14:24:42 +0800

    spider beautifulgirl

--- Spider/spider_beautifulgirl.py
+++ Spider/spider_beautifulgirl.py
@@ -7,30 +7,27 @@ class SpiderPictures:
     #获取所有图片
     def get_allPictures(self,_url):
         #文件保存位置
-        fileLocation = os.getcwd()
+        fileLocation = os.getcwd()+'/beautifulgirl'
         html = requests.get(url=_url).text
         soup = BeautifulSoup(html,'html.parser')
         pic_urls = soup.find_all(target='_blank')
-
-        i=0
+        i = 1
         for url in pic_urls:
-            print( url.decode() + '\n')
 
-            pic = requests.get(url)
+            pic = requests.get(url.img['src'])
             # 如果文件夹不存在，则创建一个文件夹
-            if not os.path.isdir():
+            if not os.path.isdir(fileLocation):
                 os.makedirs(fileLocation)
+                print(fileLocation)
 
-            fp = open(fileLocation + str(i) + '.jpg', 'wb')
+            fp = open(fileLocation +'/'+'Beautifulgirl'+str(i) + '.jpg', 'wb')
             fp.write(pic.content)
             # print position+each
             fp.close()
             i += 1
 
         return
-    def save_picture(self,_imgUrl):
 
-        return
 
 spider = SpiderPictures()
 spider.get_allPictures('https://m.umei.cc/umplus/search.php?q=%E5%88%B6%E6%9C%8D&pagesize=24')
